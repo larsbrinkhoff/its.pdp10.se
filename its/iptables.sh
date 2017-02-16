0e3db57cd312bb8623e499cd7639e47280612321
Lars Brinkhoff <lars@nocrew.org>
2017-02-16 09:14:38 +0000

    Allow outgoing FTP connections.

--- its/iptables.sh
+++ its/iptables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo 1 > /proc/sys/net/ipv4/ip_forward 
+modprobe nf_conntrack_ftp
+modprobe nf_nat_ftp
 
 PORTS="25 79 95"
 CHAOS=42042
@@ -16,6 +18,9 @@ done
 iptables -A FORWARD -p udp -m udp --dport $CHAOS -j ACCEPT
 
 iptables -A FORWARD -s 192.168.1.100 -p udp --dport 53 -j ACCEPT
+
+iptables -A FORWARD -s 192.168.1.100 -p tcp --syn --dport 21 -m conntrack --ctstate NEW -j ACCEPT
+iptables -t nat -A POSTROUTING -s 192.168.1.100 -p tcp --dport 21 -j SNAT --to-source 188.114.255.79
 
 iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i tun0 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
