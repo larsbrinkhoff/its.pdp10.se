ccf801e46d2647844fe8ac0c390ce2f521073bce
Lars Brinkhoff <lars@nocrew.org>
2017-02-16 12:27:19 +0000

    Generalise and simplify script.

--- its/iptables.sh
+++ its/iptables.sh
@@ -4,23 +4,27 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 modprobe nf_conntrack_ftp
 modprobe nf_nat_ftp
 
-PORTS="25 79 95"
+IP=192.168.1.100
+GW=188.114.255.79
+INCOMING="25 79 95"
+OUTGOING="21"
 CHAOS=42042
 
 iptables -F
 iptables -F -t nat
 
-for i in $PORTS; do
-    iptables -A FORWARD -i eth0 -o tun0 -p tcp --syn --dport $i -m conntrack --ctstate NEW -j ACCEPT
-    iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $i -j DNAT --to-destination 192.168.1.100
+for i in $INCOMING; do
+    iptables -A FORWARD -d $IP -p tcp --syn --dport $i -m conntrack --ctstate NEW -j ACCEPT
+    iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $i -j DNAT --to-destination $IP
+done
+
+for i in $OUTGOING; do
+    iptables -A FORWARD -s $IP -p tcp --syn --dport $i -m conntrack --ctstate NEW -j ACCEPT
+    iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport $i -j SNAT --to-source $GW
 done
 
 iptables -A FORWARD -p udp -m udp --dport $CHAOS -j ACCEPT
 
-iptables -A FORWARD -s 192.168.1.100 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -s $IP -p udp --dport 53 -j ACCEPT
 
-iptables -A FORWARD -s 192.168.1.100 -p tcp --syn --dport 21 -m conntrack --ctstate NEW -j ACCEPT
-iptables -t nat -A POSTROUTING -s 192.168.1.100 -p tcp --dport 21 -j SNAT --to-source 188.114.255.79
-
-iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i tun0 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
